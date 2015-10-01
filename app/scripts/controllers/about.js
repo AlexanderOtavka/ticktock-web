fc5c59a9dde9493a95b05a78a7c70dbc049318ec
manmon42 <manmon42@gmail.com>
2015-09-30 19:21:43 -0700

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ticktockWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ticktockWebApp
+ */
+angular.module('ticktockWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ticktockWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ticktockWebApp
+ */
+angular.module('ticktockWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
